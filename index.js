f47b5d13dd7c733b15d29dc74e14246ebd040692
Marshall3572 <944797458@qq.com>
2022-04-28 15:53:07 +0800

    fix: cant connect to server when preview articles

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
-
+const path = require('path')
 const fetch = require('node-fetch')
 const fs = require('fs')
-const path = require('path')
 const {prompt} = require('enquirer')
 const http = require('http')
 const handler = require('serve-handler')
@@ -17,21 +16,22 @@ async function start() {
         message: '请输入想要面经的份数',
         initial: 30
     })
-    result.count = parseInt(response.count)
+    // result.count = parseInt(response.count)
+    Object.assign(result, response)
 
     response = await prompt({
-        type: 'input',
+        type: 'select',
         name: 'job',
         message: '请输入你感兴趣的岗位方向',
-        // choices: ['前端', 'Java', '后端', '测试', '产品经理', 'UI']
+        choices: ['前端', 'Java', '后端', '测试', '产品经理', 'UI']
     })
     Object.assign(result, response)
 
     response = await prompt({
-        type: 'input',
+        type: 'select',
         name: 'company',
         message: '请输入你感兴趣的公司',
-        // choices: ['阿里巴巴', '字节跳动', '腾讯', '拼多多', '百度', '华为']
+        choices: ['阿里巴巴', '字节跳动', '腾讯', '拼多多', '百度', '华为']
     })
     Object.assign(result, response)
     console.log(result)
@@ -40,31 +40,35 @@ async function start() {
     let count = 0  //已经下载的数量
     let urls = []
 
+    console.log(`正在为您寻找最符合要求的${result.count}篇面经`)
     do {
-        let url = `https://www.nowcoder.com/search?type=post&order=create&query=${result.company}+${result.job}&subType=2&tagId=&page=${page}`
+        let url = `https://www.nowcoder.com/search?type=post&subType=2&tagId=0&order=create&page=${page}&query=${result.company}+${result.job}`
         // console.log(url)
-        console.log(`正在为您寻找最符合要求的${result.count}面经`)
         let res = await fetch(url)
         let body = await res.text()
-        urls = body.match(/\/discuss\/\d+\?.+?"/g) || []
-        urls = urls.map(text => `https://www.nowcoder.com${text.replace('"', '')}`)
+        urls = body.match(/\/discuss\/\d+\?.+"/g).map(text => `https://www.nowcoder.com${text.replace('"', '')}`)
         for (let i = 0; i < urls.length; i++) {
-            console.log(`已推荐${i+1}篇`)
-            fs.writeFileSync(path.join(process.cwd() + '/articles', `${result.job}_${result.company}_${count}.html`), body, 'utf-8')
-            count++
+            let article = await fetch(urls[i])
+            let text = await article.text()
+            text = text.replace(/<script[\s\S]+?<\/script>/g, '')
+            fs.writeFileSync(path.join(process.cwd() + '/articles', `${result.job}_${result.company}_${count}.html`), text, {
+                encoding: 'utf-8',
+                flag: 'a+'
+            })
+            console.log(`已推荐${++count}篇`)
             if (count >= result.count) break
-            await new Promise(resolve => setTimeout(resolve, 200))
+            await new Promise(res => setTimeout(res, 100))
         }
         page++
     } while (count < result.count && urls.length > 0)
     console.log('推荐完毕，将自动跳转服务器预览地址')
-    await new Promise(resolve => setTimeout(resolve, 2000))
+    // await new Promise(resolve => setTimeout(resolve, 5000))
     http.createServer(async (req, res) => {
         await handler(req, res)
     }).listen(3000, async () => {
+        console.log('打开http://localhost:3000/articles')
         await open('http://localhost:3000/articles')
     })
-    console.log('打开http://localhost:3000/articles')
 }
 
 start()
